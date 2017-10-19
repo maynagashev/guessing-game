ce690127578b29e4da5c0610c355786c57beb574
maynagashev <lifesup@gmail.com>
2017-10-19 09:23:03 +0700

    + function, cycle and results

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,15 +2,24 @@
 
 echo "[Welcome to Guessing game v0.1]"
 
-
-function guess {
-
+function ask {
 	echo "Please enter the number of files in the current directory:"
-
-	read user_guess
-
-	echo "You entered: $user_guess"
+	read guess
+    files=$(ls -1 | wc -l)
 }
 
+ask
 
-guess
+while [[ $guess -ne $files ]]
+do
+	if [[ $guess -lt $files ]] 
+	then
+		echo "Too low."
+	else
+		echo "Too high."
+	fi
+	ask
+done
+
+echo "Well done! It is the correct answer, here is the list of files:"
+echo "---" && ls -1
